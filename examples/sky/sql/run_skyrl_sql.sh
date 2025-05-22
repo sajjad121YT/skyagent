sajjad121YT/skyagent

commit fee8341e3be44900a7107e844443c32113931968

Sky-RL-SQL (#13)
* Upload all files

Co-authored-by: Shu Liu <lshu@berkeley.edu>
Co-authored-by: Sumanth Hegde <sumanthrh@anyscale.com>
Co-authored-by: Shiyi Cao <shicao@berkeley.edu>
Signed-off-by: lynnliu030 <lshu@berkeley.edu>

* Update README.md

* Update README.md

* Fix typo

* cleanup

---------

Signed-off-by: lynnliu030 <lshu@berkeley.edu>
Co-authored-by: Sumanth Hegde <sumanthrh@anyscale.com>
Co-authored-by: Shiyi Cao <shicao@berkeley.edu>

.gitignore
@@ -1,0 +1,5 @@
+__pycache__
+*egg-info*
+/dataset/
+outputs*
+outputs/*

examples/sky/sql/run_skyrl_sql.sh
@@ -1,0 +1,79 @@
+export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
+export DATA_DIR='/SkyRL-SQL-653-data'
+DB_PATH=/data
+BASE_MODEL='Qwen/Qwen2.5-Coder-7B-Instruct'
+CKPT_PATH='/mnt/local_storage/ckpt'
+
+PROJECT_NAME='MultiTurn-Experiment'
+EXPERIMENT_NAME="SkyRL-SQL-7B"
+
+KL_LOSS_COEF=0.001
+ENTROPY_COEFF=0
+KL_LOSS_TYPE=low_var_kl
+N_AGENT=5
+N_TURNS=5
+TEMP=0.6
+TOPP=0.95
+USE_KL_LOSS=False
+LR=1e-6
+CLIP_LOW=0.2
+CLIP_HIGH=0.2
+GRAD_CLIP=0.5
+BATCH_SIZE=256
+TP_SIZE=4
+
+PYTHONUNBUFFERED=1 uv run --isolated --directory . --frozen --env-file .env.sql -m verl.trainer.main_ppo \
+    data.train_files=$DATA_DIR/train.parquet \
+    data.val_files=$DATA_DIR/validation.parquet \
+    data.train_batch_size=$BATCH_SIZE \
+    data.dataloader_num_workers=0 \
+    data.val_batch_size=1 \
+    data.max_prompt_length=29048 \
+    data.max_response_length=3000 \
+    actor_rollout_ref.rollout.sql.max_prompt_length=29048 \
+    actor_rollout_ref.rollout.sql.max_response_length=3000 \
+    actor_rollout_ref.rollout.sql.max_start_length=6144 \
+    actor_rollout_ref.rollout.sql.max_obs_length=3000 \
+    algorithm.adv_estimator=grpo \
+    actor_rollout_ref.model.path=$BASE_MODEL \
+    actor_rollout_ref.actor.use_kl_loss=$USE_KL_LOSS \
+    actor_rollout_ref.actor.clip_ratio_high=$CLIP_HIGH \
+    actor_rollout_ref.actor.clip_ratio_low=$CLIP_LOW \
+    actor_rollout_ref.model.enable_gradient_checkpointing=true \
+    actor_rollout_ref.actor.fsdp_config.param_offload=True \
+    actor_rollout_ref.actor.fsdp_config.optimizer_offload=True \
+    actor_rollout_ref.model.use_remove_padding=True \
+    actor_rollout_ref.actor.optim.lr=$LR \
+    actor_rollout_ref.actor.ppo_mini_batch_size=$BATCH_SIZE \
+    actor_rollout_ref.actor.ppo_micro_batch_size=8 \
+    actor_rollout_ref.actor.kl_loss_coef=$KL_LOSS_COEF \
+    actor_rollout_ref.actor.kl_loss_type=$KL_LOSS_TYPE \
+    actor_rollout_ref.actor.entropy_coeff=$ENTROPY_COEFF \
+    actor_rollout_ref.rollout.log_prob_micro_batch_size=64 \
+    actor_rollout_ref.rollout.tensor_model_parallel_size=$TP_SIZE \
+    actor_rollout_ref.rollout.n_trajectories=$N_AGENT \
+    actor_rollout_ref.rollout.max_iterations=$N_TURNS \
+    actor_rollout_ref.rollout.name=async \
+    actor_rollout_ref.rollout.enable_memory_saver=True \
+    actor_rollout_ref.rollout.task_type='sql' \
+    reward_model.reward_manager=sql \
+    actor_rollout_ref.rollout.sql.db_path=$DB_PATH \
+    actor_rollout_ref.rollout.gpu_memory_utilization=0.9 \
+    actor_rollout_ref.ref.log_prob_micro_batch_size=64 \
+    actor_rollout_ref.ref.fsdp_config.param_offload=True \
+    actor_rollout_ref.rollout.sampling_params.temperature=$TEMP \
+    actor_rollout_ref.rollout.sampling_params.top_p=$TOPP \
+    actor_rollout_ref.actor.masking=true \
+    trainer.logger=['wandb'] \
+    trainer.n_gpus_per_node=8 \
+    trainer.nnodes=1 \
+    trainer.save_freq=5 \
+    trainer.test_freq=-1 \
+    trainer.project_name=$PROJECT_NAME \
+    trainer.experiment_name=$EXPERIMENT_NAME \
+    trainer.total_epochs=20 \
+    trainer.default_local_dir=$CKPT_PATH/$PROJECT_NAME/$EXPERIMENT_NAME \
+    trainer.max_actor_ckpt_to_keep=10 \
+    trainer.max_critic_ckpt_to_keep=10 \
+    actor_rollout_ref.actor.grad_clip=$GRAD_CLIP \
+    2>&1 

examples/sky/swebench/run_skyrl_agent_oh7b_s1.sh
@@ -1,12 +1,12 @@
 PROJECT_NAME='oh-7b-training'
-EXPERIMENT_NAME='SkyRL-Agent-7b-v0-stage2'
+EXPERIMENT_NAME='SkyRL-Agent-7b-v0-stage1'
 DATA_PATH="<path_to_swegym_dataset>"
-SFT_MODEL_PATH='PATH_FROM_STAGE1'
+SFT_MODEL_PATH='NovaSky-AI/SWE-Gym-OpenHands-7B-Agent'
 CKPT_PATH='<path_to_ckpt>'
 
 
 BATCH_SIZE=16
-MAX_NUM_ITERS=25
+MAX_NUM_ITERS=15
 NUM_TRAJ=16
 MAX_PARALLEL_AGENTS=64
 SAVE_FREQ=1
@@ -29,7 +29,7 @@ TOP_P=0.95
 
 
 
-PYTHONUNBUFFERED=1 uv run --isolated --directory . --frozen --env-file .env -m verl.trainer.main_ppo \
+PYTHONUNBUFFERED=1 uv run --isolated --directory . --frozen --env-file .env.swebench -m verl.trainer.main_ppo \
     algorithm.adv_estimator=grpo \
     data.train_files=["$DATA_PATH/train.parquet"] \
     data.val_files=["$DATA_PATH/validation.parquet"] \

examples/sky/swebench/run_skyrl_agent_oh7b_s2.sh
@@ -1,12 +1,12 @@
 PROJECT_NAME='oh-7b-training'
-EXPERIMENT_NAME='SkyRL-Agent-7b-v0-stage1'
+EXPERIMENT_NAME='SkyRL-Agent-7b-v0-stage2'
 DATA_PATH="<path_to_swegym_dataset>"
-SFT_MODEL_PATH='NovaSky-AI/SWE-Gym-OpenHands-7B-Agent'
+SFT_MODEL_PATH='PATH_FROM_STAGE1'
 CKPT_PATH='<path_to_ckpt>'
 
 
 BATCH_SIZE=16
-MAX_NUM_ITERS=15
+MAX_NUM_ITERS=25
 NUM_TRAJ=16
 MAX_PARALLEL_AGENTS=64
 SAVE_FREQ=1
@@ -29,7 +29,7 @@ TOP_P=0.95
 
 
 
-PYTHONUNBUFFERED=1 uv run --isolated --directory . --frozen --env-file .env -m verl.trainer.main_ppo \
+PYTHONUNBUFFERED=1 uv run --isolated --directory . --frozen --env-file .env.swebench -m verl.trainer.main_ppo \
     algorithm.adv_estimator=grpo \
     data.train_files=["$DATA_PATH/train.parquet"] \
     data.val_files=["$DATA_PATH/validation.parquet"] \

examples/sky/swebench/run_skyrl_agent_qwen14b_t.sh
@@ -1,48 +1,47 @@
-PROJECT_NAME='oh-training-qwen3-no-thinking-8b-grpo'
-EXPERIMENT_NAME='oh-training-qwen3-8b-32bsz-16r-temp0.6-25turn-easy'
+PROJECT_NAME='oh-training-qwen3-14b-ppo'
+EXPERIMENT_NAME='oh-training-qwen3-14b-32bsz-16r-temp0.6-clip-0.28-35turn'
 DATA_PATH="<path_to_swegym_dataset>"
-SFT_MODEL_PATH='Qwen/Qwen3-8B' 
+SFT_MODEL_PATH='Qwen/Qwen3-14B' 
+CRITIC_MODEL_PATH=$SFT_MODEL_PATH
 CKPT_PATH='<path_to_ckpt>'
 
-
 BATCH_SIZE=32
-MAX_NUM_ITERS=25
+MAX_NUM_ITERS=35
 NUM_TRAJ=8
-MAX_PARALLEL_AGENTS=64
+MAX_PARALLEL_AGENTS=192
 SAVE_FREQ=1
 
 USE_KL_LOSS=True
 KL_LOSS_COEF=0.001
 KL_LOSS_TYPE=low_var_kl
-ENTROPY_COEFF=0
+ENTROPY_COEFF=0.001
 CLIP_RATIO_LOW=0.2
-CLIP_RATIO_HIGH=0.2
+CLIP_RATIO_HIGH=0.28
 
-GPU_MEM_UTIL=0.8
-TP_SIZE=1
 # Assumes a h200 node
 # For 2xH100: change tp size -> 2, sequence parallel size -> 2, nnodes -> 2
+GPU_MEM_UTIL=0.8
+TP_SIZE=2
 NNODES=1
 SP_SIZE=1
+
+
 TEMPERATURE=0.6
 TOP_P=0.95
 
-
-
-PYTHONUNBUFFERED=1 uv run --isolated --directory . --frozen --env-file .env -m verl.trainer.main_ppo \
-    algorithm.adv_estimator=grpo \
+PYTHONUNBUFFERED=1 uv run --isolated --directory . --frozen --env-file .env.swebench -m verl.trainer.main_ppo \
+    algorithm.adv_estimator=gae \
     data.train_files=["$DATA_PATH/train.parquet"] \
     data.val_files=["$DATA_PATH/validation.parquet"] \
     data.train_batch_size=$BATCH_SIZE \
-    data.max_prompt_length=31232 \
-    data.max_response_length=1536 \
+    data.max_prompt_length=31744 \
+    data.max_response_length=3072 \
     data.truncation='error' \
     actor_rollout_ref.model.path=$SFT_MODEL_PATH \
     actor_rollout_ref.actor.optim.lr=1e-6 \
     actor_rollout_ref.model.use_remove_padding=True \
     actor_rollout_ref.actor.ppo_mini_batch_size=$BATCH_SIZE \
     actor_rollout_ref.actor.ppo_micro_batch_size_per_gpu=1 \
-    actor_rollout_ref.actor.ulysses_sequence_parallel_size=$SP_SIZE \
     actor_rollout_ref.actor.use_kl_loss=$USE_KL_LOSS \
     actor_rollout_ref.actor.kl_loss_coef=$KL_LOSS_COEF \
     actor_rollout_ref.actor.kl_loss_type=$KL_LOSS_TYPE \
@@ -50,8 +49,17 @@ PYTHONUNBUFFERED=1 uv run --isolated --directory . --frozen --env-file .env -m v
     actor_rollout_ref.model.enable_gradient_checkpointing=True \
     actor_rollout_ref.actor.fsdp_config.param_offload=True \
     actor_rollout_ref.actor.fsdp_config.optimizer_offload=True \
+    actor_rollout_ref.actor.ulysses_sequence_parallel_size=$SP_SIZE \
     actor_rollout_ref.actor.clip_ratio_low=$CLIP_RATIO_LOW \
     actor_rollout_ref.actor.clip_ratio_high=$CLIP_RATIO_HIGH \
+    critic.optim.lr=1e-5 \
+    critic.ulysses_sequence_parallel_size=$SP_SIZE \
+    critic.model.use_remove_padding=True \
+    critic.model.path=$CRITIC_MODEL_PATH \
+    critic.model.enable_gradient_checkpointing=True \
+    critic.ppo_micro_batch_size_per_gpu=1 \
+    critic.model.fsdp_config.param_offload=True \
+    critic.model.fsdp_config.optimizer_offload=True \
     actor_rollout_ref.rollout.log_prob_micro_batch_size_per_gpu=1 \
     actor_rollout_ref.rollout.tensor_model_parallel_size=$TP_SIZE \
     actor_rollout_ref.rollout.name=async \
@@ -62,13 +70,11 @@ PYTHONUNBUFFERED=1 uv run --isolated --directory . --frozen --env-file .env -m v
     actor_rollout_ref.rollout.temperature=$TEMPERATURE \
     actor_rollout_ref.rollout.sampling_params.top_p=$TOP_P \
     actor_rollout_ref.rollout.top_p=$TOP_P \
-    actor_rollout_ref.rollout.sampling_params.top_k=20 \
-    actor_rollout_ref.rollout.sampling_params.repetition_penalty=1.5 \
     actor_rollout_ref.rollout.max_parallel_agents=$MAX_PARALLEL_AGENTS \
     actor_rollout_ref.rollout.max_iterations=$MAX_NUM_ITERS \
     actor_rollout_ref.rollout.enable_memory_saver=True \
-    actor_rollout_ref.rollout.qwen3_enable_thinking=False \
     +actor_rollout_ref.rollout.max_starting_message_length=12000 \
+    actor_rollout_ref.rollout.remove_think_tokens=True \
     actor_rollout_ref.actor.masking=True \
     actor_rollout_ref.ref.log_prob_micro_batch_size_per_gpu=1 \
     actor_rollout_ref.ref.fsdp_config.param_offload=True \

examples/sky/swebench/run_skyrl_agent_qwen8b_nt.sh
@@ -1,47 +1,48 @@
-PROJECT_NAME='oh-training-qwen3-14b-ppo'
-EXPERIMENT_NAME='oh-training-qwen3-14b-32bsz-16r-temp0.6-clip-0.28-35turn'
+PROJECT_NAME='oh-training-qwen3-no-thinking-8b-grpo'
+EXPERIMENT_NAME='oh-training-qwen3-8b-32bsz-16r-temp0.6-25turn-easy'
 DATA_PATH="<path_to_swegym_dataset>"
-SFT_MODEL_PATH='Qwen/Qwen3-14B' 
-CRITIC_MODEL_PATH=$SFT_MODEL_PATH
+SFT_MODEL_PATH='Qwen/Qwen3-8B' 
 CKPT_PATH='<path_to_ckpt>'
 
+
 BATCH_SIZE=32
-MAX_NUM_ITERS=35
+MAX_NUM_ITERS=25
 NUM_TRAJ=8
-MAX_PARALLEL_AGENTS=192
+MAX_PARALLEL_AGENTS=64
 SAVE_FREQ=1
 
 USE_KL_LOSS=True
 KL_LOSS_COEF=0.001
 KL_LOSS_TYPE=low_var_kl
-ENTROPY_COEFF=0.001
+ENTROPY_COEFF=0
 CLIP_RATIO_LOW=0.2
-CLIP_RATIO_HIGH=0.28
+CLIP_RATIO_HIGH=0.2
 
+GPU_MEM_UTIL=0.8
+TP_SIZE=1
 # Assumes a h200 node
 # For 2xH100: change tp size -> 2, sequence parallel size -> 2, nnodes -> 2
-GPU_MEM_UTIL=0.8
-TP_SIZE=2
 NNODES=1
 SP_SIZE=1
-
-
 TEMPERATURE=0.6
 TOP_P=0.95
 
-PYTHONUNBUFFERED=1 uv run --isolated --directory . --frozen --env-file .env -m verl.trainer.main_ppo \
-    algorithm.adv_estimator=gae \
+
+
+PYTHONUNBUFFERED=1 uv run --isolated --directory . --frozen --env-file .env.swebench -m verl.trainer.main_ppo \
+    algorithm.adv_estimator=grpo \
     data.train_files=["$DATA_PATH/train.parquet"] \
     data.val_files=["$DATA_PATH/validation.parquet"] \
     data.train_batch_size=$BATCH_SIZE \
-    data.max_prompt_length=31744 \
-    data.max_response_length=3072 \
+    data.max_prompt_length=31232 \
+    data.max_response_length=1536 \
     data.truncation='error' \
     actor_rollout_ref.model.path=$SFT_MODEL_PATH \
     actor_rollout_ref.actor.optim.lr=1e-6 \
     actor_rollout_ref.model.use_remove_padding=True \
     actor_rollout_ref.actor.ppo_mini_batch_size=$BATCH_SIZE \
     actor_rollout_ref.actor.ppo_micro_batch_size_per_gpu=1 \
+    actor_rollout_ref.actor.ulysses_sequence_parallel_size=$SP_SIZE \
     actor_rollout_ref.actor.use_kl_loss=$USE_KL_LOSS \
     actor_rollout_ref.actor.kl_loss_coef=$KL_LOSS_COEF \
     actor_rollout_ref.actor.kl_loss_type=$KL_LOSS_TYPE \
@@ -49,17 +50,8 @@ PYTHONUNBUFFERED=1 uv run --isolated --directory . --frozen --env-file .env -m v
     actor_rollout_ref.model.enable_gradient_checkpointing=True \
     actor_rollout_ref.actor.fsdp_config.param_offload=True \
     actor_rollout_ref.actor.fsdp_config.optimizer_offload=True \
-    actor_rollout_ref.actor.ulysses_sequence_parallel_size=$SP_SIZE \
     actor_rollout_ref.actor.clip_ratio_low=$CLIP_RATIO_LOW \
     actor_rollout_ref.actor.clip_ratio_high=$CLIP_RATIO_HIGH \
-    critic.optim.lr=1e-5 \
-    critic.ulysses_sequence_parallel_size=$SP_SIZE \
-    critic.model.use_remove_padding=True \
-    critic.model.path=$CRITIC_MODEL_PATH \
-    critic.model.enable_gradient_checkpointing=True \
-    critic.ppo_micro_batch_size_per_gpu=1 \
-    critic.model.fsdp_config.param_offload=True \
-    critic.model.fsdp_config.optimizer_offload=True \
     actor_rollout_ref.rollout.log_prob_micro_batch_size_per_gpu=1 \
     actor_rollout_ref.rollout.tensor_model_parallel_size=$TP_SIZE \
     actor_rollout_ref.rollout.name=async \
@@ -70,11 +62,13 @@ PYTHONUNBUFFERED=1 uv run --isolated --directory . --frozen --env-file .env -m v
     actor_rollout_ref.rollout.temperature=$TEMPERATURE \
     actor_rollout_ref.rollout.sampling_params.top_p=$TOP_P \
     actor_rollout_ref.rollout.top_p=$TOP_P \
+    actor_rollout_ref.rollout.sampling_params.top_k=20 \
+    actor_rollout_ref.rollout.sampling_params.repetition_penalty=1.5 \
     actor_rollout_ref.rollout.max_parallel_agents=$MAX_PARALLEL_AGENTS \
     actor_rollout_ref.rollout.max_iterations=$MAX_NUM_ITERS \
     actor_rollout_ref.rollout.enable_memory_saver=True \
+    actor_rollout_ref.rollout.qwen3_enable_thinking=False \
     +actor_rollout_ref.rollout.max_starting_message_length=12000 \
-    actor_rollout_ref.rollout.remove_think_tokens=True \
     actor_rollout_ref.actor.masking=True \
     actor_rollout_ref.ref.log_prob_micro_batch_size_per_gpu=1 \
     actor_rollout_ref.ref.fsdp_config.param_offload=True \

commit 74b6db41cb72fc5fc7a050a371118c2d9f95b4b6

Fix sglang dependency conflicts for Swebench and SQL tasks (#17)
Fixes an SGLang dependency conflict issue for Swebench and SQL tasks.

Currently, sglang 0.4.6.post1 is needed for swebench training - this is to support Qwen3 models.
However, with the SQL task, with offline generate API, we've noticed that using 0.4.6.post1 leads to a 10x slowdown in generation time compared to 0.4.4.post1.

This PR does the following:

This PR does the following: 
- Uses extras for swebench and sql. 
- Swebench and Sql install their own versions of sglang. 
- Fixes an import error for swebench task introduced after #13 : Import guarding `CodeActAgentGroup` will fail with:
```bash
File "/home/ray/.cache/uv/builds-v0/.tmpgUt252/lib/python3.12/site-packages/scantree/__init__.py", line 7, in <module>
(TaskRunner pid=2880621)     __version__ = _version.get_versions()["version"]
(TaskRunner pid=2880621)                   ^^^^^^^^^^^^^^^^^^^^^^^
(TaskRunner pid=2880621)   File "/home/ray/.cache/uv/builds-v0/.tmpgUt252/lib/python3.12/site-packages/scantree/_version.py", line 670, in get_versions
(TaskRunner pid=2880621)     pieces = git_pieces_from_vcs(cfg.tag_prefix, root, verbose)
(TaskRunner pid=2880621)              ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
(TaskRunner pid=2880621)   File "/home/ray/.cache/uv/builds-v0/.tmpgUt252/lib/python3.12/site-packages/scantree/_version.py", line 384, in git_pieces_from_vcs
(TaskRunner pid=2880621)     date = date.splitlines()[-1]
(TaskRunner pid=2880621)            ~~~~~~~~~~~~~~~~~^^^^
(TaskRunner pid=2880621) IndexError: list index out of range
```
the fix is to import at the file-level. 
- Updates lock file is to handle both extras. 

---------

Signed-off-by: SumanthRH <sumanthrh@anyscale.com>

.gitignore
@@ -1,5 +1,4 @@
 __pycache__
-*egg-info*
+*.egg-info/
 /dataset/
-outputs*
-outputs/*
+/outputs/

examples/sky/sql/run_skyrl_sql.sh
@@ -22,7 +22,7 @@ GRAD_CLIP=0.5
 BATCH_SIZE=256
 TP_SIZE=4
 
-PYTHONUNBUFFERED=1 uv run --isolated --directory . --frozen --env-file .env.sql -m verl.trainer.main_ppo \
+PYTHONUNBUFFERED=1 uv run --extra sql --isolated --directory . --frozen --env-file .env.sql -m verl.trainer.main_ppo \
     data.train_files=$DATA_DIR/train.parquet \
     data.val_files=$DATA_DIR/validation.parquet \
     data.train_batch_size=$BATCH_SIZE \

examples/sky/swebench/run_skyrl_agent_oh7b_s1.sh
@@ -29,7 +29,7 @@ TOP_P=0.95
 
 
 
-PYTHONUNBUFFERED=1 uv run --isolated --directory . --frozen --env-file .env.swebench -m verl.trainer.main_ppo \
+PYTHONUNBUFFERED=1 uv run --isolated --extra swebench --directory . --frozen --env-file .env.swebench -m verl.trainer.main_ppo \
     algorithm.adv_estimator=grpo \
     data.train_files=["$DATA_PATH/train.parquet"] \
     data.val_files=["$DATA_PATH/validation.parquet"] \

examples/sky/swebench/run_skyrl_agent_oh7b_s2.sh
@@ -29,7 +29,7 @@ TOP_P=0.95
 
 
 
-PYTHONUNBUFFERED=1 uv run --isolated --directory . --frozen --env-file .env.swebench -m verl.trainer.main_ppo \
+PYTHONUNBUFFERED=1 uv run --isolated --directory . --frozen --extra swebench --env-file .env.swebench -m verl.trainer.main_ppo \
     algorithm.adv_estimator=grpo \
     data.train_files=["$DATA_PATH/train.parquet"] \
     data.val_files=["$DATA_PATH/validation.parquet"] \

examples/sky/swebench/run_skyrl_agent_qwen14b_t.sh
@@ -29,7 +29,7 @@ SP_SIZE=1
 TEMPERATURE=0.6
 TOP_P=0.95
 
-PYTHONUNBUFFERED=1 uv run --isolated --directory . --frozen --env-file .env.swebench -m verl.trainer.main_ppo \
+PYTHONUNBUFFERED=1 uv run --isolated --extra swebench --directory . --frozen --env-file .env.swebench -m verl.trainer.main_ppo \
     algorithm.adv_estimator=gae \
     data.train_files=["$DATA_PATH/train.parquet"] \
     data.val_files=["$DATA_PATH/validation.parquet"] \

examples/sky/swebench/run_skyrl_agent_qwen8b_nt.sh
@@ -29,7 +29,7 @@ TOP_P=0.95
 
 
 
-PYTHONUNBUFFERED=1 uv run --isolated --directory . --frozen --env-file .env.swebench -m verl.trainer.main_ppo \
+PYTHONUNBUFFERED=1 uv run --isolated --extra swebench --directory . --frozen --env-file .env.swebench -m verl.trainer.main_ppo \
     algorithm.adv_estimator=grpo \
     data.train_files=["$DATA_PATH/train.parquet"] \
     data.val_files=["$DATA_PATH/validation.parquet"] \

pyproject.toml
@@ -16,7 +16,7 @@ name = "verl"
 # We'll mark the version as "dynamic" because it's read from the file "verl/version/version" 
 # (PEP 621 calls this "dynamic version"). 
 # The actual version is specified in the [tool.setuptools.dynamic] section below.
-dynamic = ["version", "optional-dependencies", "authors", "urls"]
+dynamic = ["version"]
 
 description = "verl: Volcano Engine Reinforcement Learning for LLM"
 license = {file = "LICENSE"}  # or "Apache-2.0", if you prefer an SPDX identifier
@@ -44,8 +44,6 @@ dependencies=[
   "hf_transfer",
   "torchdata",
   "openhands-ai",
-  'sglang[all]==0.4.4.post4', 
-  "flash-attn@https://github.com/Dao-AILab/flash-attention/releases/download/v2.7.4.post1/flash_attn-2.7.4.post1+cu12torch2.5cxx11abifalse-cp312-cp312-linux_x86_64.whl",
   "streamlit",
   "whatthepatch",
   "retry",
@@ -65,13 +63,37 @@ dependencies=[
   "browsergym-visualwebarena==0.10.2",
   'tensordict<=0.6.2',
   'torch-memory-saver>=0.0.5',
-  'vllm>=0.7.3'
+  'vllm>=0.7.3',
 ]
 
 [tool.uv]
-find-links = ["https://flashinfer.ai/whl/cu124/torch2.5/flashinfer-python"]
-override-dependencies = ["ray==2.43.0", "flash-attn@https://github.com/Dao-AILab/flash-attention/releases/download/v2.7.4.post1/flash_attn-2.7.4.post1+cu12torch2.5cxx11abifalse-cp312-cp312-linux_x86_64.whl", "xgrammar==0.1.17"]
+override-dependencies = ["ray==2.43.0", "xgrammar==0.1.17"]
+conflicts = [
+    [
+        { extra = "swebench" },
+        { extra = "sql" },
+    ],
+]
 
+# NOTE (sumanthrh): This is a hack pending more investigation.
+# Currently, sglang 0.4.6.post1 is needed for swebench training - this is to support Qwen3 models 
+# However, with the SQL task, we've noticed that using 0.4.6.post1 leads to a 10x slowdown in generation time compared to 0.4.4.post1. 
+# Dependency management with sglang is messy at the moment. I've not been able to track down the root cause yet. 
+[project.optional-dependencies]
+swebench = [
+    'sglang[all]==0.4.6.post1', 
+    # We explictly use a flashinfer wheel from their index because the wheels on PyPI don't come with pre-compiled kernels and the package will JIT compile them at runtime (very slow).
+    "flashinfer-python@https://github.com/flashinfer-ai/flashinfer/releases/download/v0.2.3/flashinfer_python-0.2.3+cu124torch2.6-cp38-abi3-linux_x86_64.whl#sha256=cbc4acb4aed7c4b00887a557dac484315327a063e25ac1ed4ee8eef54edf9ef6",
+    # Use flash attention with torch 2.6 since sglang 0.4.6.post1 requires torch 2.6
+    "flash-attn@https://github.com/Dao-AILab/flash-attention/releases/download/v2.7.4.post1/flash_attn-2.7.4.post1+cu12torch2.6cxx11abifalse-cp312-cp312-linux_x86_64.whl"
+]
+sql = [
+    'sglang[all]==0.4.4.post4', 
+    "flashinfer-python@https://github.com/flashinfer-ai/flashinfer/releases/download/v0.2.3/flashinfer_python-0.2.3+cu124torch2.5-cp38-abi3-linux_x86_64.whl#sha256=74fe5bf85068e12cf93100f47b932ed0c7a8547ec93905f62eab604dccbb68bd",
+    # Use flash attention with torch 2.5 since sglang 0.4.4.post4 requires torch 2.5
+    "flash-attn@https://github.com/Dao-AILab/flash-attention/releases/download/v2.7.4.post1/flash_attn-2.7.4.post1+cu12torch2.5cxx11abifalse-cp312-cp312-linux_x86_64.whl"
+]
+test = ['pytest', 'yapf', 'py-spy']
 
 [tool.uv.sources]
 openhands-ai = { path = "./SkyRL-OpenHands", editable = true }

setup.py
@@ -1,66 +1,0 @@
-# Copyright 2024 Bytedance Ltd. and/or its affiliates
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-# setup.py is the fallback installation script when pyproject.toml does not work
-from setuptools import setup, find_packages
-import os
-
-version_folder = os.path.dirname(os.path.join(os.path.abspath(__file__)))
-
-with open(os.path.join(version_folder, 'verl/version/version')) as f:
-    __version__ = f.read().strip()
-
-TEST_REQUIRES = ['pytest', 'yapf', 'py-spy']
-# PRIME_REQUIRES = ['pyext']
-GEO_REQUIRES = ['mathruler']
-GPU_REQUIRES = ['liger-kernel', 'flash-attn']
-MATH_REQUIRES = ['math-verify']  # Add math-verify as an optional dependency
-VLLM_REQUIRES = ['tensordict<=0.6.2', 'vllm<=0.8.2']
-# SGLANG_REQUIRES = [
-#   'tensordict<=0.6.2', 
-#   'sglang[all]==0.4.4.post3',
-#   'torch-memory-saver>=0.0.5'
-# ]
-
-extras_require = {
-  'test': TEST_REQUIRES,
-  # 'prime': PRIME_REQUIRES,
-  'geo': GEO_REQUIRES,
-  'gpu': GPU_REQUIRES,
-  'math': MATH_REQUIRES,
-  'vllm': VLLM_REQUIRES,
-  # 'sglang': SGLANG_REQUIRES,
-}
-
-from pathlib import Path
-this_directory = Path(__file__).parent
-long_description = (this_directory / "README.md").read_text()
-
-setup(
-    name='verl',
-    version=__version__,
-    package_dir={'': '.'},
-    packages=find_packages(where='.'),
-    url='https://github.com/volcengine/verl',
-    license='Apache 2.0',
-    author='Bytedance - Seed - MLSys',
-    author_email='zhangchi.usc1992@bytedance.com, gmsheng@connect.hku.hk',
-    description='verl: Volcano Engine Reinforcement Learning for LLM',
-    extras_require=extras_require,
-    package_data={'': ['version/*'],
-                  'verl': ['trainer/config/*.yaml'],},
-    include_package_data=True,
-    long_description=long_description,
-    long_description_content_type='text/markdown'
-)

verl/workers/agentic/async_rollout.py
@@ -1,6 +1,5 @@
-# TODO(haoran): stuck in the loop
-# TODO(haoran): time control; loss_mask
-# TODO(haoran): check reason for loading weight
+
+# Modified from: https://github.com/volcengine/verl/pull/917 
 import logging
 import os
 from functools import partial
@@ -16,6 +15,9 @@ from sglang.srt.function_call_parser import FunctionCallParser
 from sglang.srt.openai_api.protocol import Tool
 from torch.distributed import DeviceMesh
 from tensordict import TensorDict
+# NOTE (sumanthrh): Ideally, we can guard this import and run it only for the swe bench task. 
+# However, in openhands' long chain of dependencies, the import for `scantree` fails when this is done.
+from verl.workers.agentic.swe_agent.codeact import CodeActAgentGroup
 
 from verl import DataProto
 from verl.workers.rollout.base import BaseRollout
@@ -96,7 +98,6 @@ class AsyncRollout(BaseRollout):
             raise ValueError("Sampling parameter `n` is not supported for multi-turn agentic tasks. For generating multiple trajectories per instance, please use `rollout.n_trajectories` instead.")
         
         if self.config.task_type == "swegym":
-            from verl.workers.agentic.swe_agent.codeact import CodeActAgentGroup
             codeact_agent_group = CodeActAgentGroup(
                 batch=prompts,
                 num_trajectories=self.config.n_trajectories,
@@ -104,8 +105,8 @@ class AsyncRollout(BaseRollout):
                 max_prompt_length=self.config.prompt_length,
                 max_response_length=self.config.response_length,
                 max_starting_message_length=self.config.max_starting_message_length,
-                max_parallel_agents=self.config.max_parallel_agents // self.device_mesh.size(0),
-                max_eval_parallel_agents=self.config.max_eval_parallel_agents // self.device_mesh.size(0),
+                max_parallel_agents=max(self.config.max_parallel_agents // self.device_mesh.size(0), 1),
+                max_eval_parallel_agents=max(self.config.max_eval_parallel_agents // self.device_mesh.size(0), 1),
                 max_iterations = self.config.max_iterations,
                 tokenizer=self.engine.tokenizer_manager.tokenizer,
                 sampling_params=sampling_params,
@@ -117,33 +118,28 @@ class AsyncRollout(BaseRollout):
 
             results = codeact_agent_group.run()
         elif self.config.task_type == "sql":
-            try: 
-                from verl.workers.agentic.llm_sql_agent.sqlact import SQLActAgentGroup
-                from verl.workers.agentic.llm_sql_agent.generation import GenerationConfig
-                total_world_size = torch.distributed.get_world_size()
-                gen_config = GenerationConfig(
-                    max_turns=self.config.max_iterations,
-                    max_start_length=self.config.sql.max_start_length,
-                    max_prompt_length=self.config.sql.max_prompt_length,
-                    max_response_length=self.config.sql.max_response_length,
-                    max_obs_length=self.config.sql.max_obs_length,
-                    num_gpus= total_world_size // self.device_mesh.size(0),
-                    db_path=self.config.sql.db_path,
-                    no_think_rl=False,
-                )
-                agent_group = SQLActAgentGroup(
-                    batch=prompts,
-                    infer_engine=self.engine,
-                    num_trajectories=self.config.n_trajectories,
-                    gen_config=gen_config,
-                    tokenizer=self.engine.tokenizer_manager.tokenizer, 
-                    sampling_params=self.sampling_params,
-                )
-                results = agent_group.run()
-            except Exception as e:
-                print(f"DEBUG: Encountered an exception: {e}")
-                print(f"Traceback: \n\n\n{traceback.print_exc()}")
-                raise e
+            from verl.workers.agentic.llm_sql_agent.sqlact import SQLActAgentGroup
+            from verl.workers.agentic.llm_sql_agent.generation import GenerationConfig
+            total_world_size = torch.distributed.get_world_size()
+            gen_config = GenerationConfig(
+                max_turns=self.config.max_iterations,
+                max_start_length=self.config.sql.max_start_length,
+                max_prompt_length=self.config.sql.max_prompt_length,
+                max_response_length=self.config.sql.max_response_length,
+                max_obs_length=self.config.sql.max_obs_length,
+                num_gpus= total_world_size // self.device_mesh.size(0),
+                db_path=self.config.sql.db_path,
+                no_think_rl=False,
+            )
+            agent_group = SQLActAgentGroup(
+                batch=prompts,
+                infer_engine=self.engine,
+                num_trajectories=self.config.n_trajectories,
+                gen_config=gen_config,
+                tokenizer=self.engine.tokenizer_manager.tokenizer, 
+                sampling_params=self.sampling_params,
+            )
+            results = agent_group.run()
         else:
             raise NotImplementedError(f"Task type {self.task_type} is not supported.")
         logger.info(f"nodedup finish generate seq {torch.distributed.get_rank()=} {self.tp_rank=}")

verl/workers/agentic/swe_agent/codeact.py
@@ -482,7 +482,7 @@ class OnlineCodeActAgent(Agent):
                 actions = codeact_function_calling.response_to_actions(
                     self.convert_str_to_completion_format(fn_call_messages)
                 )
-                print(f"Take action: {type(actions)}")
+                print(f"Take action: {[type(action) for action in actions]}")
                 
                 for action in actions:
                     self.pending_actions.append(action)